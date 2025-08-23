b9c0e1d32181739408485d469e093d303169ae50
Olga Shomarova <olga.shomarova@protonmail.com>
2025-08-23 11:35:44 +0300

    Cat task

---
+++ Module_1/src/Cat.java
@@ -0,0 +1,24 @@
+public class Cat {
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public void meow() {
+        System.out.println("The cat named " + name + " says: Meow!");
+    }
+
+    public static void main(String[] args) {
+        // Create an instance of the Cat class with a name
+        Cat cat = new Cat("Whiskers");
+        Cat anotherCat = new Cat("Rex");
+
+
+        // Call the meow method on the cat instance
+        cat.meow();
+        cat.meow();
+        anotherCat.meow();
+        cat.meow();
+    }
+}
